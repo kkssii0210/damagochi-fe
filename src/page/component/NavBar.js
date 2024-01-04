6176e7f6ee468f6d1bcef8041a48f4378d1c126c
yel106 <yel_@hanmail.net>
2024-01-05 00:32:24 +0900

    임의의 Main과 NavBar

---
+++ src/page/component/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Flex } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+export function NavBar(props) {
+  const navigate = useNavigate();
+
+  return (
+    <Flex>
+      <Button variant="ghost" size="lg" onClick={() => navigate("/")}>
+        Home
+      </Button>
+      <Button variant="ghost" size="lg" onClick={() => navigate("/item/list")}>
+        상점가기
+      </Button>
+    </Flex>
+  );
+}
+
+export default NavBar;
