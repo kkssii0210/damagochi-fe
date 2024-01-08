802bd9229528073e38a541eeafddd10cd558eea4
jeon602 <731aa@naver.com>
2024-01-08 17:55:21 +0900

    다마고치 인포 & 다마고치 튜토리얼에 관한 조건 만들기

---
+++ src/gameTutorial/MongTutorial.js
@@ -0,0 +1,11 @@
+// plan: damagochi's evolution level 다마고치 Mong의 진화 단계 설명서 페이지
+
+import React from 'react';
+
+function MongTutorial(props) {
+  return (
+    <div></div>
+  );
+}
+
+export default MongTutorial;
