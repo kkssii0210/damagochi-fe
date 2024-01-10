65846b418414ab64ce640a45a9a74123a685f422
yel106 <yel_@hanmail.net>
2024-01-10 12:39:47 +0900

    리스트에서 아이템 총 갯수 표시, 아이템 수정 기능

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ import ItemList from "./page/store/Item/ItemList";
 import HomeLayout from "./page/component/HomeLayout";
 import ItemView from "./page/store/Item/ItemView";
 import Main from "./page/component/Main";
+import ItemEdit from "./page/store/Item/ItemEdit";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomeLayout />}>
       <Route index element={<Main />} />
-      <Route path="store/item/view" element={<ItemView />} />
       <Route path="store/item/register" element={<ItemRegister />} />
       <Route path="store/item/list" element={<ItemList />} />
+      <Route path="store/item/view/id/:storeId" element={<ItemView />} />
+      <Route path="store/item/edit/id/:storeId" element={<ItemEdit />} />
     </Route>,
   ),
 );
