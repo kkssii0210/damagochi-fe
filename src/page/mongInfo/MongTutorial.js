348a77678831e3f1a9d6aaa86c734bba823f3122
jeon602 <731aa@naver.com>
2024-01-11 15:51:59 +0900

    Ｍｏｎｇ ｔｕｔｏｒｉａｌｓ ｅｄｉｔｔｉｎｇ。。。。。。

--- src/page/mongInfo/MongTutorial.js
+++ src/page/mongInfo/MongTutorial.js
@@ -1,13 +1,68 @@
 // plan: damagochi's evolution level 다마고치 Mong의 진화 단계 설명서 페이지
 
-import React, {useEffect, useRef} from 'react';
+import {
+  Text,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  SimpleGrid
+} from "@chakra-ui/react";
+import React from "react";
 
 export function MongTutorial(props) {
-
-
   return (
-    <div></div>
-  );
+    <>
+      <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+        <Card border="1px solid">
+          <CardHeader>
+            <Heading size='md'> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size='md'> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size='md'> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size='md'> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
+    </>
+  )
 }
 
 export default MongTutorial;
