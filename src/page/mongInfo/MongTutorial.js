3595e3540b4d4e112b21759174141a10e691efb5
jeon602 <731aa@naver.com>
2024-01-11 17:48:01 +0900

    3. Ｍｏｎｇ ｔｕｔｏｒｉａｌｓ ｅｄｉｔｔｉｎｇ。。。。。。

--- src/page/mongInfo/MongTutorial.js
+++ src/page/mongInfo/MongTutorial.js
@@ -8,43 +8,58 @@ import {
   CardFooter,
   CardHeader,
   Heading,
-  SimpleGrid
+  SimpleGrid, Box, Image
 } from "@chakra-ui/react";
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAngleUp} from "@fortawesome/free-solid-svg-icons";
+import Step1Damagochi from "../../알.gif";
+import Step2Damagochi from "../../자아생성시기.gif";
+import Step3Damagochi from "../../사춘기.gif";
+import Step4Damagochi from "../../다큼.gif";
+import styles from "../../WelcomePage.module.css";
 
 export function MongTutorial(props) {
   return (
     <>
-      <SimpleGrid border="1px solid" spacing={4} columns={4}>
-        <Card border="1px solid">
+      <SimpleGrid border="1px solid" spacing={1} columns={7} margin="flex" >
+        <Card>
           <CardHeader>
+            <img src={Step1Damagochi} alt="step1" className={styles.step} />
           </CardHeader>
           <CardBody>
             <Text>전희연</Text>
           </CardBody>
         </Card>
-        <Card border="1px solid">
+        <FontAwesomeIcon size="5x" icon={faAngleUp} rotation={90} style={{color: "#3178f2",}} />
+        <Card >
           <CardHeader>
+            <img src={Step2Damagochi} alt="step1" className={styles.step} />
           </CardHeader>
           <CardBody>
             <Text>전희연</Text>
           </CardBody>
         </Card>
-        <Card border="1px solid">
+        <FontAwesomeIcon size="5x" icon={faAngleUp} rotation={90} style={{color: "#3178f2",}} />
+        <Card>
           <CardHeader>
+            <img src={Step3Damagochi} alt="step1" className={styles.step} />
           </CardHeader>
           <CardBody>
             <Text>전희연</Text>
           </CardBody>
         </Card>
-        <Card border="1px solid">
+        <FontAwesomeIcon size="5x" icon={faAngleUp} rotation={90} style={{color: "#3178f2",}} />
+        <Card >
           <CardHeader>
+            <img src={Step4Damagochi} alt="step1" className={styles.step} />
           </CardHeader>
           <CardBody>
             <Text>전희연</Text>
           </CardBody>
         </Card>
       </SimpleGrid>
+      <FontAwesomeIcon size="5x" icon={faAngleUp} rotation={90} style={{color: "#3178f2",}} />
     </>
   )
 }
