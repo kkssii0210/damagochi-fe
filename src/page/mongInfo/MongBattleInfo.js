1b45930e0e318ed719df2317dcf1261d1487cb52
jeon602 <731aa@naver.com>
2024-01-18 18:36:18 +0900

    수정 시작.

--- src/page/mongInfo/MongBattleInfo.js
+++ src/page/mongInfo/MongBattleInfo.js
@@ -1,27 +1,11 @@
+import React, {useState} from 'react';
 import styles from "../../WelcomePage.module.css";
-import React, {useEffect, useState} from 'react';
-import {Box, Card, Center, Table, Th, Thead, Tr} from "@chakra-ui/react";
-import {useNavigate} from "react-router-dom";
-import axios from "axios";
-//     mongId;health;tired; defense;strength;agility;debuff;win;lose;
-export function MongBattleInfo() {
-    useD
-  const navigate= useNavigate();
+
+export function MongBattleInfo(props) {
   const [mong, setMong] = useState();
-  const [health, setHealth] = useState();
-    useEffect(() => {
-        axios
-            .get("/api/MongInfo/id")
-            .then((response)=> {setHealth(response.data);})
-            .catch((error)=> console.error(error))
-    }, []);
   return (
-    <div >
-        {/*className={styles.container}*/}
-        <Box border="1px solid orange" gap={5} justifyContent="space-evenly" display="flex">
-        <Box border="1px solid skyblue">{mong.health}</Box>
-        <Box border="1px solid skyblue"></Box>
-        </Box>
+    <div className={styles.container}>
+
     </div>
   );
 }
