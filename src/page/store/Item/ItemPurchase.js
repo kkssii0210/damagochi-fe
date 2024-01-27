50f11218f882b1f0fbac66f7c53dd4d63cdf28ce
yel106 <yel_@hanmail.net>
2024-01-27 16:26:52 +0900

    구매하기 화면

--- src/page/store/Item/ItemPurchase.js
+++ src/page/store/Item/ItemPurchase.js
@@ -4,12 +4,17 @@ import {
   Container,
   Heading,
   HStack,
+  Table,
+  Td,
   Text,
+  Th,
+  Tr,
   VStack,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import response from "sockjs-client/lib/event/trans-message";
 
 // 실제 카드결제를 해서 포인트를 결제하는건 payment.js
 // 결제된 포인트를 이용해 상점에서 아이템을 구매하는건 purchase.js
@@ -19,60 +24,115 @@ import { useParams } from "react-router-dom";
 // response에 로그인한 멤버의 아이디, 자기포인트, itemPrice를 가져와야함
 // 멤버 아이디 가져오기
 export function ItemPurchase() {
+  const navigate = useNavigate();
   const [purchaseInfo, setPurchaseInfo] = useState(null);
   const { storeId } = useParams();
-  const [myPoint, setMyPoint] = useState(null);
-  const [itemPrice, setItemPrice] = useState(null);
+
+  const [member, setMember] = useState({ playerId: "" });
+  const [cartInfo, setCartInfo] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(null);
   const [remainingPoint, setRemainingPoint] = useState(null);
 
-    useEffect(() => {
-        if (localStorage.getItem("accessToken") !== null) {
-            console.log(localStorage.getItem("accessToken"));
-            axios
-                .get("/api/purchase/accessToken", {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-                    },
-                })
-                .then((response) => {
-                    localStorage.setItem("accessToken", response.data.accessToken);
-                    console.log(response.data.accessToken);
-                })
-                .catch()
-                .finally();
-        }
-    }, []);
+  useEffect(() => {
+    if (localStorage.getItem("accessToken") !== null) {
+      console.log(localStorage.getItem("accessToken"));
+      axios
+        .get("/api/purchase/accessToken", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          setMember(response.data);
+          handleGetCartItem(response.data.playerId, response.data.point);
+        })
+        .catch()
+        .finally();
+    }
+  }, []);
 
-    return (
-        <Container
-            mt={20}
-            border="3px solid black"
-            borderRadius="20px"
-            variant="ghost"
-        >
-            <VStack>
-                <Heading mt={8} mb={8}>
-                    아이템 구매
-                </Heading>
+  function handleGetCartItem(playerId, point) {
+    axios
+      .get("/api/purchase/cartInfo", {
+        params: { playerId: playerId },
+      })
+      .then((response) => {
+        setCartInfo(response.data);
+        const totalPrice = response.data.reduce(
+          (acc, item) => acc + item.cartItemPrice,
+          0,
+        );
+        setTotalPrice(totalPrice);
+        const remainPoint = point - totalPrice;
+        setRemainingPoint(remainPoint);
+      })
+      .catch((error) => {
+        console.log("카트 아이템의 정보를 가져오는데 실패하였습니다.");
+      })
+      .finally();
+  }
 
-                <HStack mb={3}>
-                    <Text>현재 내 포인트</Text>
-                    <Text>-</Text>
-                    <Text>아이템 가격</Text>
-                    <Text>=</Text>
-                    <Text>남은 포인트</Text>
-                </HStack>
-                <HStack mb={10}>
-                    <Text>포인트{}</Text>
-                    <Text>-</Text>
-                    <Text>가격{}</Text>
-                    <Text>=</Text>
-                    <Text>포인트</Text>
-                </HStack>
-                <Button colorScheme="purple" mb={20}>
-                    결제하기
-                </Button>
-            </VStack>
-        </Container>
-    );
+  function handlePurchaseItem() {}
+
+  const playerIdWithoutAt = member.playerId.split("@")[0];
+
+  return (
+    // Member: 포인트, item: price
+    <Container
+      mt={20}
+      border="0px solid black"
+      borderRadius="20px"
+      variant="ghost"
+    >
+      <VStack>
+        <Heading mt={8} mb={20}>
+          {playerIdWithoutAt}님의 구매 목록
+        </Heading>
+
+        <Table mb={10} border={"0px solid blue"}>
+          <Tr>
+            <Th textAlign={"center"} fontSize={"medium"}>
+              목록 번호
+            </Th>
+            <Th textAlign={"center"} fontSize={"medium"}>
+              아이템명
+            </Th>
+            <Th textAlign={"center"} fontSize={"medium"}>
+              아이템 가격
+            </Th>
+          </Tr>
+          {cartInfo.map((cartItem, index) => (
+            <Tr>
+              <Td textAlign={"center"}>{index + 1}</Td>
+              <Td textAlign={"center"}>{cartItem.cartItemName}</Td>
+              <Td textAlign={"center"}>{cartItem.cartItemPrice} 포인트</Td>
+            </Tr>
+          ))}
+        </Table>
+
+        <Table mb={10} mt={5} border={"0px solid blue"}>
+          <Tr>
+            <Th fontSize={"large"} padding={"10px"}>
+              현재 내 포인트
+            </Th>
+            <Th fontSize={"large"} padding={"10px"}>
+              아이템의 총 가격
+            </Th>
+            <Th fontSize={"large"} padding={"10px"}>
+              구매 후 포인트
+            </Th>
+          </Tr>
+          <Tr>
+            <Td>{member.point} 포인트</Td>
+            <Td>{totalPrice} 포인트</Td>
+            <Td>{remainingPoint} 포인트</Td>
+          </Tr>
+        </Table>
+        <Button colorScheme="purple" mb={20} onClick={handlePurchaseItem}>
+          아이템 구매하기
+        </Button>
+      </VStack>
+    </Container>
+  );
 }
