12170a698cc3ff7dcb03b8e9ef54dd352942bc41
hrhrhrasd <gmleh1245@gmail.com>
2024-02-01 02:06:56 +0900

    인벤토리 이미지

--- src/page/management/Inventory.js
+++ src/page/management/Inventory.js
@@ -34,11 +34,13 @@ export function Inventory({memberId, mystyle, onClick, onClose}) {
         <div style={{background : "lightblue", width : "100%", height : "100%"}}>
         <h1>인벤토리</h1>
         <SimpleGrid columns={3} spacing={10} mt={"15px"} style={{overflow : "hidden"}}>
-            {items.map((item) => (
+            {items
+                .filter(item => item.category === 'liquidMedicine')
+                .map((item) => (
                 <div key={item.id} style={{ width: "100%", marginBottom: "20px", position: "relative" }}
                      onClick={()=>handleClick(item)}
                 >
-                    <img src={Step1Damagochi} alt={item.name} style={{ width: "70%", height: "auto" }} />
+                    <img src={item.image} alt={item.name} style={{ width: "70%", height: "auto" }} />
                     <div style={{ position: "absolute", bottom: "20px", right: "5px", background: "rgba(0,0,0,0.7)", color: "white", padding: "5px", borderRadius: "5px" }}>
                         <span style={{ marginRight: "3px" }}>{item.quantity}</span>
                     </div>
